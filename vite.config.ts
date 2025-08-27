0fd14d61404b8c9a313aa04813fb9075f1a65f31
BR-64 <smooot.stu@gmail.com>
2025-08-27 10:57:25 +0700

    dev environment for liff

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    allowedHosts: [
+      '8964308fbc5f.ngrok-free.app', // 👈 add your ngrok domain here
+      'dress-issued-harbour-brooklyn.trycloudflare.com', // 👈 add your cloudflare domain here
+      'gzip-jeff-des-ra.trycloudflare.com',
+      'speeds-thumbs-newly-arm.trycloudflare.com',
+    ],
+  },
 });
